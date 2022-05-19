fecca59eefac507fd495309680971764ba233ee9
Carlos Bello <bellocarlos2810@gmail.com>
2022-05-18 23:21:53 -0500

    generate project structure

---
+++ src/main/java/com/salcedoFawcett/services/economicProposal/EconomicProposalApplication.java
@@ -0,0 +1,13 @@
+package com.salcedoFawcett.services.economicProposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomicProposalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salcedoFawcett/services/economicProposal/EconomicProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salcedoFawcett.services.economicProposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
